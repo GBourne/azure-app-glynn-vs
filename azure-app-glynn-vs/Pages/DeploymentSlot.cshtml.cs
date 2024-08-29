654624596b63f5b9972a47725454bd601d172c72
Glynn Bourne <glynnbourne@gmail.com>
2024-08-29 11:42:28 +0100

    Added Deloyment Slots

---
+++ azure-app-glynn-vs/Pages/DeploymentSlot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_glynn_vs.Pages
+{
+    public class DeploymentSlotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
